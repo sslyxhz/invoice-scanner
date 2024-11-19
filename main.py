7edd247fcdf25432765693677d016b3d4e6deb76
zxh <zxh@gxar.com>
2024-11-19 16:21:20 +0800

    feat: update

--- main.py
+++ main.py
@@ -1,9 +1,38 @@
 import sys
-from PySide6.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QListWidget, QLabel, QFileDialog
+from PySide6.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QListWidget, QLabel, QFileDialog, QProgressDialog
 from PySide6.QtGui import QPixmap
+from PySide6.QtCore import Qt, QThread, Signal
 from rapidocr_onnxruntime import RapidOCR
 
 engine = RapidOCR()
+
+class RecognitionThread(QThread):
+    progressUpdated = Signal(int)
+    finished = Signal()
+
+    def __init__(self, imageList, resultList):
+        super().__init__()
+        self.imageList = imageList
+        self.resultList = resultList
+
+    def run(self):
+        imageListSize = self.imageList.count()
+        for i in range(imageListSize):
+            imgPath = self.imageList.item(i).text()
+            result, elapse = engine(imgPath, use_det=True, use_cls=False, use_rec=True) # 检测+识别
+            texts = [item[1] for item in result]
+            target_text = '号码'
+            for text in texts:
+                if text.startswith(target_text):
+                    # print(text) # 号码：01819689
+                    numbers = ''.join(filter(str.isdigit, text))
+                    print(numbers)
+                    self.resultList.addItem(numbers)
+                    
+                    progress = (i + 1) * 100 // imageListSize
+                    self.progressUpdated.emit(progress)
+        self.finished.emit()
+
 
 class MainWindow(QWidget):
     def __init__(self):
@@ -52,17 +81,27 @@ class MainWindow(QWidget):
             
 
     def recognizeImage(self):
-        for i in range(self.imageList.count()):
-            imgPath = self.imageList.item(i).text()
-            result, elapse = engine(imgPath, use_det=True, use_cls=False, use_rec=True) # 检测+识别
-            texts = [item[1] for item in result]
-            target_text = '号码'
-            for text in texts:
-                if text.startswith(target_text):
-                    # print(text) # 号码：01819689
-                    numbers = ''.join(filter(str.isdigit, text))
-                    print(numbers)
-                    self.resultList.addItem(numbers)
+        self.loading_dialog = QProgressDialog("正在识别...", "取消", 0, 100, self)
+        self.loading_dialog.setWindowModality(Qt.WindowModal)
+        self.loading_dialog.show()
+        
+        self.recognitionThread = RecognitionThread(self.imageList, self.resultList)
+        self.recognitionThread.progressUpdated.connect(self.updateProgressDialog)
+        self.recognitionThread.finished.connect(self.recognitionFinished)
+        self.recognitionThread.start()
+        
+        # for i in range(self.imageList.count()):
+        #     imgPath = self.imageList.item(i).text()
+        #     result, elapse = engine(imgPath, use_det=True, use_cls=False, use_rec=True) # 检测+识别
+        #     texts = [item[1] for item in result]
+        #     target_text = '号码'
+        #     for text in texts:
+        #         if text.startswith(target_text):
+        #             # print(text) # 号码：01819689
+        #             numbers = ''.join(filter(str.isdigit, text))
+        #             print(numbers)
+        #             self.resultList.addItem(numbers)
+                    
         # img_path = 'test.jpg'
         # result, elapse = engine(img_path, use_det=True, use_cls=False, use_rec=True) # 检测+识别
         # texts = [item[1] for item in result]
@@ -73,7 +112,17 @@ class MainWindow(QWidget):
         #         numbers = ''.join(filter(str.isdigit, text))
         #         print(numbers)
         #         self.resultList.addItem(numbers)
-
+        
+        
+        
+    def updateProgressDialog(self, progress):
+        self.loading_dialog.setValue(progress)
+        
+        
+    def recognitionFinished(self):
+        self.loading_dialog.close()
+        
+        
     def cleanAll(self):
         self.imageList.clear()
         self.resultList.clear()
