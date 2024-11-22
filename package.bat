b2ac8f2907213125636a4b29be37797402b66076
sslyxhz <sslyxhz@hotmail.com>
2024-11-22 23:10:34 +0800

    feat: update package error

--- package.bat
+++ package.bat
@@ -1,5 +1,4 @@
-# pyinstaller --onefile main.py 
 pyinstaller --onefile ^
     --add-data "C:\tools\Miniconda3\envs\invoice\Lib\site-packages\rapidocr_onnxruntime\config.yaml;rapidocr_onnxruntime" ^
     --add-data "C:\tools\Miniconda3\envs\invoice\Lib\site-packages\rapidocr_onnxruntime\models;rapidocr_onnxruntime\models" ^
-    main.py 
+    main.py
